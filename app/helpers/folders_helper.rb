e89041a5236150f76df047b41e08c9d22857460c
Mischa Berger <mischa78@xs4all.nl>
2010-12-23 13:07:13 +0900

    Different files types have different icons

--- app/helpers/folders_helper.rb
+++ app/helpers/folders_helper.rb
@@ -4,4 +4,12 @@ module FoldersHelper
     breadcrumbs = breadcrumbs(folder.parent, breadcrumbs) unless folder.parent == Folder.root
     breadcrumbs.html_safe
   end
+
+  def file_icon(extension)
+    if FileTest.exists?("#{Rails.root}/public/images/fileicons/#{extension}.png")
+      "fileicons/#{extension}.png"
+    else
+      'file.png'
+    end
+  end
 end
--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -7,4 +7,8 @@ class UserFile < ActiveRecord::Base
   validates_attachment_presence :attachment, :message => "can't be blank"
   validates_uniqueness_of :attachment_file_name, :scope => 'folder_id', :message => 'exists already'
   validates_format_of :attachment_file_name, :with => /^[^\/\\\?\*:|"<>]+$/, :message => 'cannot contain any of these characters: < > : " / \ | ? *'
+
+  def extension
+    File.extname(attachment_file_name)[1..-1]
+  end
 end
