e40c1e32e85d2ed46683c0338ed495165ee9d118
Evgeny Lapin <lev@openteam.ru>
2011-02-11 11:10:02 +0600

    fix reset password error

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,7 @@
 class UserMailer < ActionMailer::Base
 
+  default :from => "boxroom@example.com"
+
   def reset_password_email(user)
     @user = user
     mail(:to => user.email, :subject => '[Boxroom] Password reset instructions')
