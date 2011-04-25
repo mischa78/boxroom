297f12ecc83cbb6177cc18144b72c0d002dee2f5
Mischa Berger <mischa78@xs4all.nl>
2011-04-25 17:19:07 +0900

    Made the duration of the javascript slide-up/down effect shorter

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,10 +14,10 @@ function fadeout(element)
 function show_permissions()
 {
 	if($('permissions').visible()) { show_folder(); return; }
-	if($('files_and_folders').visible()) { Effect.BlindUp('files_and_folders'); }
-	if($('clipboard').visible()) { Effect.BlindUp('clipboard'); }
+	if($('files_and_folders').visible()) { Effect.BlindUp('files_and_folders', { duration: 0.5 }); }
+	if($('clipboard').visible()) { Effect.BlindUp('clipboard', { duration: 0.5 }); }
 
-	Effect.BlindDown('permissions', { delay: 1.0 });
+	Effect.BlindDown('permissions', { delay: 0.5, duration: 0.5 });
 
 	$('show_permissions_link').className = 'highlight';
 	$('show_clipboard_link').className = 'folder_menu';
@@ -25,10 +25,10 @@ function show_permissions()
 
 function show_folder()
 {
-	if($('permissions') != undefined && $('permissions').visible()) { Effect.BlindUp('permissions'); }
-	if($('clipboard').visible()) { Effect.BlindUp('clipboard'); }
+	if($('permissions') != undefined && $('permissions').visible()) { Effect.BlindUp('permissions', { duration: 0.5 }); }
+	if($('clipboard').visible()) { Effect.BlindUp('clipboard', { duration: 0.5 }); }
 
-	Effect.BlindDown('files_and_folders', { delay: 1.0 });
+	Effect.BlindDown('files_and_folders', { delay: 0.5, duration: 0.5 });
 
 	if($('show_permissions_link') != undefined) { $('show_permissions_link').className = 'folder_menu'; }
 	$('show_clipboard_link').className = 'folder_menu';
@@ -37,10 +37,10 @@ function show_folder()
 function show_clipboard()
 {
 	if($('clipboard').visible()) { show_folder(); return; }
-	if($('permissions') != undefined && $('permissions').visible()) { Effect.BlindUp('permissions'); }
-	if($('files_and_folders').visible()) { Effect.BlindUp('files_and_folders'); }
+	if($('permissions') != undefined && $('permissions').visible()) { Effect.BlindUp('permissions', { duration: 0.5 }); }
+	if($('files_and_folders').visible()) { Effect.BlindUp('files_and_folders', { duration: 0.5 }); }
 
-	Effect.BlindDown('clipboard', { delay: 1.0 });
+	Effect.BlindDown('clipboard', { delay: 0.5, duration: 0.5 });
 
 	$('show_clipboard_link').className = 'highlight';
 	if($('show_permissions_link') != undefined) { $('show_permissions_link').className = 'folder_menu'; }
