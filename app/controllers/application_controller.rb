5d52aa88648df698e9bc241d03c75f1b5b0ad424
Mischa Berger <mischa78@xs4all.nl>
2011-05-03 11:29:58 +0900

    Small change to cookie login check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,18 +30,17 @@ class ApplicationController < ActionController::Base
 
   def require_login
     if current_user.nil?
-      unless cookies[:auth_token].blank?
-        user = User.find_by_remember_token(cookies[:auth_token])
-      end
-      if user 
-        user.refresh_remember_token
-        session[:user_id] = user.id
-        cookies[:auth_token] = user.remember_token
-      else
+      user = User.find_by_remember_token(cookies[:auth_token]) unless cookies[:auth_token].blank?
+
+      if user.nil?
         reset_session
         session[:user_id] = nil
         session[:return_to] = request.fullpath
         redirect_to new_session_url
+      else
+        user.refresh_remember_token
+        session[:user_id] = user.id
+        cookies[:auth_token] = user.remember_token
       end
     end
   end
