09a2598c75933f31728560edd8130cd23e82bcf6
Mischa Berger <mischa78@xs4all.nl>
2011-05-03 13:38:43 +0900

    Added translations for error messages in UserFile model

--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -3,9 +3,9 @@ class UserFile < ActiveRecord::Base
 
   belongs_to :folder
 
-  validates_attachment_presence :attachment, :message => "can't be blank"
-  validates_uniqueness_of :attachment_file_name, :scope => 'folder_id', :message => 'exists already'
-  validates_format_of :attachment_file_name, :with => /^[^\/\\\?\*:|"<>]+$/, :message => 'cannot contain any of these characters: < > : " / \ | ? *'
+  validates_attachment_presence :attachment, :message => I18n.t(:blank, :scope => [:activerecord, :errors, :messages])
+  validates_uniqueness_of :attachment_file_name, :scope => 'folder_id', :message => I18n.t(:exists_already, :scope => [:activerecord, :errors, :messages])
+  validates_format_of :attachment_file_name, :with => /^[^\/\\\?\*:|"<>]+$/, :message => I18n.t(:invalid_characters, :scope => [:activerecord, :errors, :messages])
 
   def copy(target_folder)
     new_file = self.clone
