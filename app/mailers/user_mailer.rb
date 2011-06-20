d3adbdde4c208bcb35b60cbe793d7238a2cf25fd
Mischa Berger <mischa78@xs4all.nl>
2011-06-20 13:08:22 +0900

    Removed space at the end of the line

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   def reset_password_email(user)
     @user = user
-    mail(:to => user.email, :subject => t(:reset_password_email_subject)) 
+    mail(:to => user.email, :subject => t(:reset_password_email_subject))
   end
 
   def download_link_email(user, share_link)
