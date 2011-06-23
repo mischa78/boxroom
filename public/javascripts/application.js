4a5f269f352265901029928ef52cfd1e32835c40
Mischa Berger <mischa78@xs4all.nl>
2011-06-23 17:38:16 +0900

    If an element ('view') is visible already, the files_and_folders view will be shown.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,19 +13,21 @@ function fadeout(element)
 
 function show_element(element)
 {
+	if($(element).visible()) { element = 'files_and_folders'; }
+
 	elements = ['files_and_folders', 'permissions', 'clipboard'];
 	elements.splice(elements.indexOf(element), 1);
 
 	elements.each(function(e) {
 		if($(e) != undefined && $(e).visible()) { Effect.BlindUp(e, { duration: 0.5 }); }
-	})
+	});
 
 	Effect.BlindDown(element, { delay: 0.5, duration: 0.5 });
 	if($('show_' + element + '_link') != undefined) { $('show_' + element + '_link').className = 'highlight'; }
 
 	elements.each(function(e) {
 		if($('show_' + e + '_link') != undefined) { $('show_' + e + '_link').className = 'folder_menu'; }
-	})
+	});
 }
 
 function update_counter(element, counter)
