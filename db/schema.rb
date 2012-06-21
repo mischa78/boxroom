20073988f7dcc86a023614bf08392907bf528aa2
Mischa Berger <mischa78@xs4all.nl>
2012-06-22 01:07:21 +0900

    Apparently created_at and updated_at cannot be null anymore

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -15,14 +16,14 @@ ActiveRecord::Schema.define(:version => 20120411081345) do
   create_table "folders", :force => true do |t|
     t.string   "name"
     t.integer  "parent_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "groups", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "groups_users", :id => false, :force => true do |t|
@@ -44,8 +45,8 @@ ActiveRecord::Schema.define(:version => 20120411081345) do
     t.string   "link_token"
     t.datetime "link_expires_at"
     t.integer  "user_file_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "user_files", :force => true do |t|
@@ -54,8 +55,8 @@ ActiveRecord::Schema.define(:version => 20120411081345) do
     t.integer  "attachment_file_size"
     t.datetime "attachment_updated_at"
     t.integer  "folder_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
   end
 
   create_table "users", :force => true do |t|
@@ -67,8 +68,8 @@ ActiveRecord::Schema.define(:version => 20120411081345) do
     t.string   "remember_token"
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
     t.string   "signup_token"
     t.datetime "signup_token_expires_at"
   end
