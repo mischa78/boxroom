6867424fd7cee9c9ed390c12b4cf83fa41accc6e
Patrick Fratczak <patrick.fratczak@gmail.com>
2013-06-26 23:19:27 +0200

    Add custom message and user to share links; Add more infos to share links emails and index

---
+++ db/migrate/20130626210927_add_columns_message_user_id_to_share_links.rb
@@ -0,0 +1,6 @@
+class AddColumnsMessageUserIdToShareLinks < ActiveRecord::Migration
+  def change
+	add_column  :share_links, :message, :text
+   	add_column  :share_links, :user_id, :integer
+  end
+end
