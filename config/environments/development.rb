525b52ce86c703c033e0733c0599de67b8481843
Patrick Fratczak <patrick.fratczak@gmail.com>
2013-06-26 23:46:58 +0200

    Oups, made a mistake by rm cached these files

---
+++ config/environments/development.rb
@@ -0,0 +1,43 @@
+Boxroom::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  config.assets.debug = true
+  
+  # Mail settings
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address => 'mailhost',
+  #   :port => 587,
+  #   :user_name => 'user_name',
+  #   :password => 'password',
+  #   :authentication => 'plain'
+  # }
+
+  # Host
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # From address
+  # ActionMailer::Base.default :from => 'Boxroom <yourname@yourdomain.com>'
+end
---
+++ db/schema.rb
@@ -0,0 +1,81 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20130626210927) do
+
+  create_table "folders", force: true do |t|
+    t.string   "name"
+    t.integer  "parent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "groups_users", id: false, force: true do |t|
+    t.integer "group_id"
+    t.integer "user_id"
+  end
+
+  create_table "permissions", force: true do |t|
+    t.integer "folder_id"
+    t.integer "group_id"
+    t.boolean "can_create"
+    t.boolean "can_read"
+    t.boolean "can_update"
+    t.boolean "can_delete"
+  end
+
+  create_table "share_links", force: true do |t|
+    t.string   "emails"
+    t.string   "link_token"
+    t.datetime "link_expires_at"
+    t.integer  "user_file_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "message"
+    t.integer  "user_id"
+  end
+
+  create_table "user_files", force: true do |t|
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
+    t.integer  "attachment_file_size"
+    t.datetime "attachment_updated_at"
+    t.integer  "folder_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "hashed_password"
+    t.string   "password_salt"
+    t.boolean  "is_admin",                        limit: 255
+    t.string   "remember_token"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "signup_token"
+    t.datetime "signup_token_expires_at"
+  end
+
+  add_index "users", ["signup_token"], name: "index_users_on_signup_token"
+
+end
