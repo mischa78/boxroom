8e0cb2d6091a3f97c5011ec80d4a26d27be428e0
Mischa Berger <mischa78@xs4all.nl>
2013-06-28 17:49:55 +0900

    Fixed bug in migration

--- db/migrate/20130628082245_populate_user_id_in_share_links.rb
+++ db/migrate/20130628082245_populate_user_id_in_share_links.rb
@@ -1,6 +1,8 @@
 class PopulateUserIdInShareLinks < ActiveRecord::Migration
   def change
-    user = User.where.not(:name => nil).first
-    ShareLink.where(:user_id => nil).update_all(:user_id => user.id)
+    if User.any? && ShareLink.any?
+      user = User.where.not(:name => nil).first
+      ShareLink.where(:user_id => nil).update_all(:user_id => user.id)
+    end
   end
 end
