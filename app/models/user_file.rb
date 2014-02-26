06fcfedb513376c57facb285615057dd6b5041a8
Mischa Berger <mischa78@xs4all.nl>
2014-02-26 10:15:24 +0900

    Prepare for using Paperclip 4

--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -1,5 +1,6 @@
 class UserFile < ActiveRecord::Base
   has_attached_file :attachment, :path => ':rails_root/uploads/:rails_env/:id/:style/:id', :restricted_characters => RESTRICTED_CHARACTERS
+  # do_not_validate_attachment_file_type :attachment
 
   belongs_to :folder
   has_many :share_links, :dependent => :destroy
